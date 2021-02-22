39a009f2a7aa212c1a5ffcc8cc3c9be7d8eb466a
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-02-22 18:38:52 +0100

    Update Component

--- crudangl-client/src/app/app.component.ts
+++ crudangl-client/src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crudangl-client';
+  disableInputs: boolean = true;
+
+  showNuova: boolean= true;
+  showModifica: boolean;
+  showAnnulla: boolean;
+  showConferma: boolean;
+  showRimuovi: boolean;
+  showCerca: boolean= true;
+  showTable: boolean= true;
+  showForms: boolean;
+
+  ricerca() {
+    this.showNuova = true;
+    this.showCerca = true;
+    this.showTable = true;
+    this.showForms = false;
+  }
+
+  aggiungi(){
+    this.showNuova = false;
+    this.showModifica=false;
+    this.showAnnulla=true;
+    this.showConferma=true;
+    this.showRimuovi=false;
+    this.showCerca=false;
+    this.showForms=true; 
+  }
 }
